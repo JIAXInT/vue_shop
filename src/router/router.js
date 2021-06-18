2999a349aae41441babeb6635331f4f265179451
贾鑫涛 <8948190+JIAXInT@user.noreply.gitee.com>
2021-06-18 10:51:46 +0800

    完成了分类功能的开发

--- src/router/router.js
+++ src/router/router.js
@@ -6,6 +6,7 @@ import Welcome from '../components/welcome.vue'
 import Users from '../components/user/users.vue'
 import Rights from '../components/power/rights.vue'
 import Roles from '../components/power/roles.vue'
+import Cate from '../components/goods/cate.vue'
 Vue.use(Router)
 
 const router = new Router({
@@ -20,7 +21,8 @@ const router = new Router({
         { path: '/Welcome', component: Welcome },
         { path: '/users', component: Users },
         { path: '/rights', component: Rights },
-        { path: '/roles', component: Roles }
+        { path: '/roles', component: Roles },
+        { path: '/categories', component: Cate }
       ]
     }
   ]
